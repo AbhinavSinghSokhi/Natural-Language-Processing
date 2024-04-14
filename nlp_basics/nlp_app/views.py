5a0bed352ed8b5390b76084a49dcdf12c45b250b
Abhinav Singh <sokhiabhinavsingh@gmail.com>
2024-04-15 01:47:33 +0530

    added nlp stats

--- nlp_basics/nlp_app/views.py
+++ nlp_basics/nlp_app/views.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 import os
 import google.generativeai as genai
 import streamlit as st
+import spacy
+
 
 load_dotenv()  # Load environment variables
 
@@ -25,10 +27,50 @@ def prompt_processing(request):
     if request.method== "POST":
         prompt= request.POST.get("prompt")
         response = get_gemini_response(prompt)
+        responsetext=[]
+        for chunk in response:
+            responsetext.append(chunk.text)
+        # print(responsetext)
+        textcorpora=""
+        for item in responsetext:
+            textcorpora+=item
+        print(textcorpora)
+
+        # Load the SpaCy English language model
+        nlp = spacy.load("en_core_web_sm")
+
+        # Sample text
+        # text = "Apple is headquartered in Cupertino, California. Steve Jobs founded Apple."
+
+        # Process the text with SpaCy
+        doc = nlp(textcorpora)
+
+        # Get the English stopwords list from SpaCy
+        stop_words = spacy.lang.en.stop_words.STOP_WORDS
+
+        # Count the total number of tokens
+        total_tokens = len(doc)
+
+        # Count the total number of stopwords
+        total_stopwords = sum(1 for token in doc if token.text.lower() in stop_words)
+
+        # Perform Named Entity Recognition (NER)
+        entities = [(ent.text, ent.label_) for ent in doc.ents]
+
+        print("Total number of tokens:", total_tokens)
+        print("Total number of stopwords:", total_stopwords)
+        for entity , labels in entities:
+            print(f"Entity: {entity} , Label: {labels}")
+        # print("Named Entities:", entities)
+
         context = {
             'prompt': prompt,
-            'response': response
+            'response': response,
+            'tokens':total_tokens,
+            'stopwords': total_stopwords,
+            'entities': entities #need to process it
         }
-        return render(request, 'response.html', context)
+        return render(request, "index.html", context)
     else:
-        return render(request, "index.html")
+        message="Error"
+        return render(request, "index.html",message)
